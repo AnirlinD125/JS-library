c9b16c4cc87afe9732b9686b15baf83e214c7117
Anirlin <anirlind@gmail.com>
2026-03-02 22:10:32 -0500

    Clean up unused lightbox and gallery code
    Removed lightbox options and slick gallery initialization.

--- AnirlinD_inm420#a3 JsLib/public/assets/js/script.js
+++ AnirlinD_inm420#a3 JsLib/public/assets/js/script.js
@@ -10,19 +10,3 @@ new kursor({
  AOS.init();
 //  text animation
 murphy.play()
-
-lightbox.option({
-      'resizeDuration': 200,
-      'wrapAround': true
-    })
-
-$(document).ready(function(){
-  $('.gallery').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-    dots: true
-  });
-});
